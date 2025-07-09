f6a529acb894ccc6f882dbe0449cd8c12b3013d2
RainAfterDark <rainafterd4rk@gmail.com>
2025-07-09 17:08:26 +0800

    Update laptop install

--- _install_laptop.sh
+++ _install_laptop.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-sudo nixos-rebuild boot --flake ~/my-nix/.#xps7590 --option 'extra-substituters' 'https://chaotic-nyx.cachix.org/' --option extra-trusted-public-keys "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+sudo nix --experimental-features "nix-command flakes" \
+  --option "extra-substituters" "https://chaotic-nyx.cachix.org/" \
+  --option "extra-trusted-public-keys" "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" \
+  run github:nix-community/disko/latest#disko-install -- \
+  --write-efi-boot-entries \
+  --flake /$HOME/my-nix#xps7590 \
+  --disk main /dev/nvme0n1
