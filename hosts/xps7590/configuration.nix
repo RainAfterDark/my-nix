81dafdf54e8144f79521c9477d93bb89bc35407b
RainAfterDark <rainafterd4rk@gmail.com>
2025-07-09 15:25:07 +0800

    Add some default programs

--- hosts/xps7590/configuration.nix
+++ hosts/xps7590/configuration.nix
@@ -1,4 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    git
+    home-manager
+    curl
+    nano
+    networkmanager
+    nmcli
+    iw
+    wpa_supplicant
+  ];
+
   system.stateVersion = "25.11";
 }
