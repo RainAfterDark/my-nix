824ddf0a0afef9266254c2e152ba684b2a0714d0
RainAfterDark <rainafterd4rk@gmail.com>
2025-07-14 16:18:23 +0800

    Update install scripts

--- _install_laptop.sh
+++ _install_laptop.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# TODO: only a basis, will get OOM'ed because disko-install
+# builds and stores the closure in RAM for some reason...
+# Run regular disko first before nixos-install...
+
 sudo nix --experimental-features "nix-command flakes" \
   --option "extra-substituters" "https://chaotic-nyx.cachix.org/" \
   --option "extra-trusted-public-keys" "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" \
