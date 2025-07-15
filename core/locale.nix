dd28c47d37e780eb2d163c617d6d11ac6dfe5da0
RainAfterDark <rainafterd4rk@gmail.com>
2025-07-15 17:21:19 +0800

    Add en_US locale

--- core/locale.nix
+++ core/locale.nix
@@ -3,6 +3,7 @@
   time.timeZone = "Asia/Manila";
   i18n.defaultLocale = "en_PH.UTF-8";
   i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
     "en_PH.UTF-8/UTF-8"
     "tl_PH.UTF-8/UTF-8"
     "ja_JP.UTF-8/UTF-8"
