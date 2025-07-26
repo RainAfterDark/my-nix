2297ae29316baca356a55cd3c8dd5f0c779c6a1f
RainAfterDark <rainafterd4rk@gmail.com>
2025-07-26 16:44:48 +0800

    (laptop) TLP tweaks

--- hosts/xps7590/configuration.nix
+++ hosts/xps7590/configuration.nix
@@ -47,6 +47,19 @@
     useTimer = true; # periodically reapply settings
   };
 
+  # TLP
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+    };
+  };
+
   # Make CPU power stats readable
   systemd.services.fix-rapl-perms = {
     description = "Fix permissions on intel_rapl energy_uj";
