fc3831ff760c675b02ea39e208c271bd726571f3
Ame <rainafterd4rk@gmail.com>
2025-09-01 09:31:54 +0800

    i hate waybar man

--- home/desktop/widgets/waybar/waybar-style.nix
+++ home/desktop/widgets/waybar/waybar-style.nix
@@ -99,7 +99,7 @@ in
 
     #idle_inhibitor {
       margin: 6px 4px 4px 0px;
-      padding: 0px 12px 0px 6px;
+      padding: 0px 14px 0px 6px;
       color: ${base07};
       background: ${base00};
       border: 2px solid ${base07};
