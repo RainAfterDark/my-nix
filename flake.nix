4d3fde13abdd91ec50cd0a482bffe4d5f0e44a9d
Ame <rainafterd4rk@gmail.com>
2025-10-09 11:21:24 +0800

    tweaks and add gun mod

--- flake.nix
+++ flake.nix
@@ -32,14 +32,6 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    wayland-pipewire-idle-inhibit = {
-      url = "github:rafaelrc7/wayland-pipewire-idle-inhibit";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     milk-grub-theme.url = "github:gemakfy/MilkGrub";
     sddm-stray-nixos.url = "github:RainAfterDark/sddm-stray-nixos";
 
