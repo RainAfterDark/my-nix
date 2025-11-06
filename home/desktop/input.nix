471c1ff26d87316389f0f0a9c0c1d2cb5f066c1b
Ame <rainafterd4rk@gmail.com>
2025-11-06 23:06:40 +0800

    IME tweaks

--- home/desktop/input.nix
+++ home/desktop/input.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.sessionVariables = {
-    LFW_IM_MODULE = "ibus";
+    GLFW_IM_MODULE = "ibus";
   };
 
   stylix.targets.fcitx5.enable = true;
@@ -9,10 +9,13 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-configtool
-    ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-configtool
+      ];
+    };
   };
 }
