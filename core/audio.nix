f7c6f99432875ab74cedfe59be536faf64e5fdf9
Ame <rainafterd4rk@gmail.com>
2025-11-22 10:54:18 +0800

    tweak (experimental) low latency audio

--- core/audio.nix
+++ core/audio.nix
@@ -6,13 +6,32 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    extraConfig.pipewire = lib.mkIf (host == "xps7590") {
-      "92-low-latency" = {
+    extraConfig = lib.mkIf (host == "xps7590") {
+      pipewire."92-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
           "default.clock.quantum" = 256;
           "default.clock.min-quantum" = 256;
           "default.clock.max-quantum" = 256;
+        };
+      };
+      pipewire-pulse."92-low-latency" = {
+        "context.properties" = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = { };
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.min.req" = "256/48000";
+          "pulse.default.req" = "256/48000";
+          "pulse.max.req" = "256/48000";
+          "pulse.min.quantum" = "256/48000";
+          "pulse.max.quantum" = "256/48000";
+        };
+        "stream.properties" = {
+          "node.latency" = "256/48000";
+          "resample.quality" = 1;
         };
       };
     };
