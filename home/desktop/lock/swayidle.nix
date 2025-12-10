9ae2c853c679b26a94b2af80d2d5224166c2b3b7
Ame <rainafterd4rk@gmail.com>
2025-12-10 22:49:59 +0800

    change swayidle to only sleep on bat

--- home/desktop/lock/swayidle.nix
+++ home/desktop/lock/swayidle.nix
@@ -9,6 +9,15 @@ let
   niriBin = "/run/current-system/sw/bin/niri";
   systemdBin = "/run/current-system/sw/bin/systemctl";
   monitors = status: "${niriBin} msg action power-${status}-monitors";
+
+  suspendScript = pkgs.writeShellScript "suspend-check" ''
+    # Check if AC is online (1 = plugged in, 0 = battery)
+    ac_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/AC/online)   
+    if [ "$ac_status" = "0" ]; then
+      ${systemdBin} suspend
+    fi
+  '';
+  sleepOnBat = "${suspendScript}";
 in
 {
   services.swayidle = {
@@ -27,25 +36,13 @@ in
     # Sleep broken on deskop
     ++ lib.optional (host != "desktop") {
       timeout = 360; # 6:00
-      command = "${systemdBin} sleep";
+      command = sleepOnBat;
     };
-    events = [
-      {
-        event = "before-sleep";
-        command = lock;
-      }
-      {
-        event = "after-resume";
-        command = monitors "on";
-      }
-      {
-        event = "lock";
-        command = monitors "off" + "; " + lock;
-      }
-      {
-        event = "unlock";
-        command = monitors "on";
-      }
-    ];
+    events = {
+      "before-sleep" = lock;
+      "after-resume" = monitors "on";
+      "lock" = monitors "off" + "; " + lock;
+      "unlock" = monitors "on";
+    };
   };
 }
