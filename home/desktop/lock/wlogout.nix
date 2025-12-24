892d419fc2876f4ee1b492d6f5e1611ab36d29d6
Ame <rainafterd4rk@gmail.com>
2025-12-24 14:02:11 +0800

    system update

--- home/desktop/lock/wlogout.nix
+++ home/desktop/lock/wlogout.nix
@@ -10,22 +10,16 @@
         keybind = "1";
       }
       {
-        label = "logout";
-        action = "niri msg action quit -s";
-        text = "Logout";
-        keybind = "2";
-      }
-      {
         label = "reboot";
         action = "systemctl reboot --no-wall";
         text = "Reboot";
-        keybind = "3";
+        keybind = "2";
       }
       {
         label = "shutdown";
         action = "systemctl poweroff --no-wall";
         text = "Shutdown";
-        keybind = "4";
+        keybind = "3";
       }
     ];
   };
