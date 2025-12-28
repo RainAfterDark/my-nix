7b4ac6321f2fb565103715adb8764599b6cdfd14
Ame <rainafterd4rk@gmail.com>
2025-12-28 15:50:32 +0800

    fix nitch pkg count on NixOS

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -1,5 +1,12 @@
 self: super: {
   aventurine-cursor = super.callPackage ./aventurine-cursor { };
+
   hellpaper = super.callPackage ./hellpaper { };
+
   stray-font = super.callPackage ./stray-font { };
+
+  # from: https://github.com/iynaix/dotfiles/blob/02e3fce049588e0cc2fb3fd5f24b67fe180bd6fb/overlays/nitch-nix-pkgs-count.patch
+  nitch = super.nitch.overrideAttrs (o: {
+    patches = (o.patches or [ ]) ++ [ ./nitch-nix-pkgs-count.patch ];
+  });
 }
