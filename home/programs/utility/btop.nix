5a6860b2313ee40721388d54daf513b4682c0f23
Ame <rainafterd4rk@gmail.com>
2026-01-17 00:14:20 +0800

    update btop for t14 (rocm support)

--- home/programs/utility/btop.nix
+++ home/programs/utility/btop.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, host, ... }:
 {
   stylix.targets.btop.enable = true;
 
   programs.btop = {
     enable = true;
-    package = pkgs.btop-cuda;
+    package = if (host == "t14") then pkgs.btop-rocm else pkgs.btop-cuda;
     settings = {
       update_ms = 500;
       rounded_corners = false;
