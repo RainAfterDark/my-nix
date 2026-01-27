03cc0b0670bfecc00b4ebd431ea521f5694c1515
Ame <rainafterd4rk@gmail.com>
2026-01-27 16:22:40 +0800

    tweak pfetch

--- home/terminal/pfetch.nix
+++ home/terminal/pfetch.nix
@@ -3,6 +3,6 @@
   home.packages = [ pkgs.pfetch-rs ];
   home.sessionVariables = {
     PF_COL1 = 2;
-    PF_INFO = "ascii title os kernel de pkgs palette";
+    PF_INFO = "ascii title os kernel uptime pkgs palette";
   };
 }
