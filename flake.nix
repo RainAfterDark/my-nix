cf2b9c259bdd56e8c7a3f6ce283865456c4dd840
Ame <rainafterd4rk@gmail.com>
2026-02-18 23:10:40 +0800

    remove garnix binary cache (causes some build failures?)

--- flake.nix
+++ flake.nix
@@ -82,7 +82,6 @@
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "https://cache.garnix.io"
       "https://cache.flox.dev"
       "https://attic.xuyh0120.win/lantian"
       "https://niri.cachix.org"
@@ -94,7 +93,6 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
