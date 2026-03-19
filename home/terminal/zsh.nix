a296b74b608f77f9b4691db4659c71ce4ff2ddf2
Ame <rainafterd4rk@gmail.com>
2026-03-19 21:34:34 +0800

    ls

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -16,9 +16,7 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      l = "eza --icons -a --group-directories-first -1";
-      ll = "eza --icons -a --group-directories-first -1 --no-user --long";
-      tree = "eza --icons --tree --group-directories-first";
+      ls = "eza -lo --no-permissions --no-user --group-directories-first --total-size --git";
     };
 
     plugins = [
