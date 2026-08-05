f0992833bf04933287e693553239a2854d5183e1
Ame <42061128+RainAfterDark@users.noreply.github.com>
2026-08-05 22:21:13 +0800

    programs.fzf options rename

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -88,11 +88,11 @@
     enableZshIntegration = true;
 
     defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
-    fileWidgetOptions = [
+    fileWidget.options = [
       "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
     ];
-    changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
-    changeDirWidgetOptions = [
+    changeDirWidget.command = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+    changeDirWidget.options = [
       "--preview 'eza --tree --color=always {} | head -200'"
     ];
 
