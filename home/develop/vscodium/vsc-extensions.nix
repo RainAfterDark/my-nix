d4157ff60ef18738caab4e0cda9f23b4cb0499b8
Ame <42061128+RainAfterDark@users.noreply.github.com>
2026-08-12 23:29:33 +0800

    vscode ext: RunOnSave

--- home/develop/vscodium/vsc-extensions.nix
+++ home/develop/vscodium/vsc-extensions.nix
@@ -17,10 +17,19 @@ let
         mktplcRef = {
           name = "nix-embedded-highlighter";
           publisher = "atomicspirit";
-          version = "0.0.1";
+          version = "0.1.3";
           hash = "sha256-KZfUaPjReHQH0XCCiejAs+0Go8WEeGiOuxjkTfSnku0=";
         };
       };
+
+  emeraldwalk.RunOnSave = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "RunOnSave";
+      publisher = "emeraldwalk";
+      version = "1.1.5";
+      hash = "sha256-kN7oQPeLJyK9GXxnSchKcUF4XIXQ+Yd7dsSHwT/ua6k=";
+    };
+  };
 in
 {
   programs.vscodium.profiles.default = {
@@ -34,6 +43,7 @@ in
 
       jonathanharty.gruvbox-material-icon-theme
       atomicspirit.nix-embedded-highlighter
+      emeraldwalk.RunOnSave
     ];
   };
 }
